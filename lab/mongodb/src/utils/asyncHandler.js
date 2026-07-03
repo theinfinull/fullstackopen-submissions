0920e7cad3f4daa2a88315fc91f802ae1ace29b6
infinull <theinfinull@gmail.com>
2026-07-03 20:32:25 +0530

    patch(mongodb): removed the lab project for mongodb

--- lab/mongodb/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * wraps an async express handler so rejected promises are forwarded to
- * next(err) automatically, removing the need for try/catch in controllers.
- */
-export function asyncHandler(fn) {
-    return function (req, res, next) {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-}
